f704577d98fdfb5ea88499b4a50bc5b263abf95a
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2024-01-04 18:26:35 -0300

    feat: frontend login form

--- frontend/src/app/(auth)/login/_components/LoginForm.tsx
+++ frontend/src/app/(auth)/login/_components/LoginForm.tsx
@@ -1,34 +1,88 @@
 'use client'
 
-import { Button, ButtonGroup, Container, FormControl, FormHelperText, FormLabel, Input } from '@chakra-ui/react'
+import { env } from '@/env'
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input
+} from '@chakra-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const FormValuesSchema = z.object({
+  email: z.string().email(),
+  loginToken: z.string().regex(/^\d{6}$/, 'Must be a number of 6 digits')
+})
+
+type FormValues = z.infer<typeof FormValuesSchema>
 
 const LoginForm = () => {
+  const [isLoadingToken, setIsLoadingToken] = useState(false)
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting, errors },
+    getValues
+  } = useForm<FormValues>({
+    resolver: zodResolver(FormValuesSchema)
+  })
+
+  const onSubmit = async (values: FormValues) => {
+    const { email, loginToken } = values
+    try {
+      const res = await axios.post(`${env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth/login/${email}`, { loginToken })
+      console.log(res)
+    } catch (error) {
+      console.log('login submit error', error)
+    }
+  }
+  console.log(errors)
   return (
     <Container marginTop={10}>
-      <form>
-        <FormControl marginBottom={5}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl marginBottom={5} isInvalid={!!errors?.email}>
           <FormLabel>Email address</FormLabel>
-          <Input type='email' placeholder='your@email.com' />
-          <FormHelperText>We'll never share your email.</FormHelperText>
+          <Input type='email' autoComplete='off' placeholder='your@email.com' {...register('email')} />
+          {errors.email ? (
+            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+          ) : (
+            <FormHelperText>We'll never share your email.</FormHelperText>
+          )}
         </FormControl>
-        <FormControl marginBottom={5}>
+        <FormControl marginBottom={5} isInvalid={!!errors?.loginToken}>
           <FormLabel>Token</FormLabel>
-          <Input type='text' />
+          <Input type='text' autoComplete='off' {...register('loginToken')} />
+          <FormErrorMessage>{errors.loginToken?.message}</FormErrorMessage>
         </FormControl>
         <ButtonGroup>
-          <Button
-            onClick={() => {
-              console.log('login')
-            }}
-          >
-            Login
+          <Button type='submit' disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting' : 'Login'}
           </Button>
           <Button
-            onClick={() => {
-              console.log('token')
+            type='button'
+            disabled={isLoadingToken}
+            onClick={async () => {
+              const email = getValues('email')
+              setIsLoadingToken(true)
+              try {
+                const res = await axios.post(`${env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth/login/${email}/code`)
+                console.log(res)
+              } catch (error) {
+                console.log('generate token error', error)
+              } finally {
+                setIsLoadingToken(false)
+              }
             }}
           >
-            Send login token
+            {isLoadingToken ? 'Requesting token' : 'Send login token'}
           </Button>
         </ButtonGroup>
       </form>
